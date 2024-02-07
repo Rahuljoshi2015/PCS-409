db6104cb71e2c9e1b777c1ae64105990cd09122e
Rahuljoshi2015 <159226038+Rahuljoshi2015@users.noreply.github.com>
2024-02-07 11:28:52 +0530

    Update P1.cpp

--- Week 2/P1.cpp
+++ Week 2/P1.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+using namespace std;
+int main()
+{ 
+    int n,key,i,j,size,k;
+   // cout<<"Enter test case\n";
+    cin>>n;
+    int t[n];
+    for(k=0;k<n;k++)
+    {
+        int c=0;
+       // cout<<"Enter array size\n";
+       cin>>size;
+        int a[size];
+        for(i=0;i<size;i++)
+        {
+          //   cout<<"Enter element\n";
+               cin>>a[i];
+        }
+        // cout<<"Enter key\n";
+        cin>>key;
+        for(i=0;i<size;i++)
+        {
+            for(j=i+1;j<size;j++)
+            {
+                if(((a[i]-a[j])==key)||((a[j]-a[i])==key))
+                c++;
+            }
+        }
+        //cout<<"count\n";
+        t[k]=c;
 
+    }
+    for(i=0;i<n;i++)
+    cout<<t[i];
+}
